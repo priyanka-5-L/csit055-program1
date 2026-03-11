24b27fa51f23d59bc5a5310c8ba1004125f31f51
priyanka-5-L <245123751055@mvsrec.edu.in>
2026-03-11 13:32:32 +0530

    Update Biggest.java

--- Biggest.java
+++ Biggest.java
@@ -1,20 +1,28 @@
 class Biggest
 {
-public static void main(String args[])
-{
-int n1=Integer.parseInt(args[0]);
-int n2=Integer.parseInt(args[1]);
-int n3=Integer.parseInt(args[2]);
-if((n1>n2)&&(n1>n3))
-{
-System.out.println(n1+" is biggest");
-}
-else if((n2>n1)&&(n2>n3))
-{
-System.out.println(n2+" is biggest");
-}
-else
-{System.out.println(n3+" is biggest");
-}
-}
+    public static void main(String args[])
+    {
+        if(args.length < 3)
+        {
+            System.out.println("Please enter three numbers");
+            return;
+        }
 
+        int n1 = Integer.parseInt(args[0]);
+        int n2 = Integer.parseInt(args[1]);
+        int n3 = Integer.parseInt(args[2]);
+
+        if((n1 > n2) && (n1 > n3))
+        {
+            System.out.println(n1 + " is biggest");
+        }
+        else if((n2 > n1) && (n2 > n3))
+        {
+            System.out.println(n2 + " is biggest");
+        }
+        else
+        {
+            System.out.println(n3 + " is biggest");
+        }
+    }
+}
